2a9241e37d3442094604f33a34566cdbd76c721a
rwu111 <150648404+rwu111@users.noreply.github.com>
2026-06-10 13:40:45 +0800

    added character card switching

--- ggst.js
+++ ggst.js
@@ -1,16 +1,48 @@
-let currentSoundtrack = null;
+const solBtn = document.getElementById("solBtn");
+const kyBtn = document.getElementById("kyBtn");
+const happyBtn = document.getElementById("happyBtn");
+const leoBtn = document.getElementById("leoBtn");
 
-function musicAndScroll(trackName, sectionId) {
-    const soundtrack = new Audio(trackName)
-    if (currentSoundtrack) {
-        currentSoundtrack.pause();
-        currentSoundtrack.currentTime = 0;  // Reset to beginning for next time
+solBtn.addEventListener("click", () => switchCharacter("Songs/findyouroneway.mp3", "sectionSol"));
+kyBtn.addEventListener("click", () => switchCharacter("Songs/theroarofthespark.mp3","sectionKy"));
+happyBtn.addEventListener("click", () => switchCharacter("Songs/drift.mp3", "sectionHappy"));
+leoBtn.addEventListener("click", () => switchCharacter("Songs/hellfire.mp3", "sectionLeo"));
+
+let currentCharacter = null;
+
+function switchCharacter(song, scrollTo) {
+    //changeSong(song);
+    let targetCharacter = document.getElementById(scrollTo);
+    changeSection(targetCharacter);
+}
+
+async function changeSection(targetCharacter) {
+    if(currentCharacter == targetCharacter) {
+        return;
     }
 
-    soundtrack.play();
-    currentSoundtrack = soundtrack;
-    let scrollTarget = document.getElementById(sectionId);
-    scrollTarget.scrollIntoView({
-        behavior: "smooth"
-    });
+    //current character slides out
+    if(currentCharacter) {
+        currentCharacter.classList.add("exiting");
+        await new Promise(r => setTimeout(r, 1000));
+        currentCharacter.classList.remove("exiting");
+        currentCharacter.style.display = "none";
+    }
+
+    //target character slides in
+    currentCharacter = targetCharacter;
+    currentCharacter.style.display = "block";
+    currentCharacter.classList.add("entering");
+    await new Promise(r => setTimeout(r, 1000));
+    currentCharacter.classList.remove("entering");
 }
+
+function main() {
+    const characters = document.querySelectorAll(".character");
+    characters.forEach((c, i) => {
+        c.style.display = (i === 0)? "block" : "none";
+    });
+    currentCharacter = characters[0];
+}
+
+document.readyState === "loading" ? document.addEventListener("DOMContentLoaded", main) : main();
